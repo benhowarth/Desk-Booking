5a53ca2f8aa5b3ca0736711b8da3d2d330853580
benhowarth <benhowarthmail@gmail.com>
2016-03-18 10:27:00 +0000

    0.0.6
    Removed datepicker
    SQL prints out staff table to testDB

--- testDB.php
+++ testDB.php
@@ -8,13 +8,25 @@
 		$host="localhost";
 		$user="root";
 		$password="";
-		$con=mysqli_connect($host,$user,$password);
+		$con=mysqli_connect($host,$user,$password,'deskbooking');
 		if(!$con) {
 			echo "<p>MySQL Server is not connected</p>";
 		} else {
             echo "<p>MySQL Server is connected</p>";
 		}
-		
+        $sql='SELECT * FROM staff';
+		$retval=mysqli_query($con,$sql);
+		if(!$retval){
+			die("Could not get data: ".mysqli_error());
+		}
+        foreach($retval as $staff){
+            echo $staff["StaffID"];
+            echo ' ';
+            echo $staff["StaffName"];
+            echo ' ';
+            echo $staff["StaffGroup"];
+            echo '<br/>';
+        }
 		
 		mysqli_close($con);
 		?>
