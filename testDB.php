182eb028fe3a9591955007c579c1c87e8471b0c7
benhowarth <benhowarthmail@gmail.com>
2016-03-18 13:49:13 +0000

    0.0.7
    Testing printing SQL queries with inner joins etc.

--- testDB.php
+++ testDB.php
@@ -19,6 +19,7 @@
 		if(!$retval){
 			die("Could not get data: ".mysqli_error());
 		}
+        echo "Staff:<br/>";
         foreach($retval as $staff){
             echo $staff["StaffID"];
             echo ' ';
@@ -27,6 +28,76 @@
             echo $staff["StaffGroup"];
             echo '<br/>';
         }
+        $sql='SELECT * FROM desks';
+		$retval=mysqli_query($con,$sql);
+		if(!$retval){
+			die("Could not get data: ".mysqli_error());
+		}
+        echo "<br/>Desks:<br/>";
+        foreach($retval as $staff){
+            echo $staff["DeskID"];
+            echo ' ';
+            echo "2F/",$staff["DeskName"];
+            echo ' ';
+            echo $staff["DeskGroup"];
+            echo '<br/>';
+        }
+    
+        $sql='SELECT * FROM bookings';
+		$retval=mysqli_query($con,$sql);
+		if(!$retval){
+			die("Could not get data: ".mysqli_error());
+		}
+        echo "<br/>Bookings:<br/>";
+        foreach($retval as $staff){
+            echo $staff["BookingID"];
+            echo ' ';
+            echo $staff["DeskID"];
+            echo ' ';
+            echo $staff["StaffID"];
+            echo ' ';
+            echo $staff["StartDate"];
+            echo ' ';
+            echo $staff["EndDate"];
+            echo '<br/>';
+        }
+    
+        $sql='SELECT bookings.BookingID, staff.StaffName, bookings.StartDate, bookings.EndDate FROM bookings INNER JOIN staff ON bookings.StaffID=staff.StaffID';
+		$retval=mysqli_query($con,$sql);
+		if(!$retval){
+			die("Could not get data: ".mysqli_error());
+		}
+        echo "<br/>Bookings:<br/>";
+        foreach($retval as $staff){
+            echo $staff["BookingID"];
+            echo ' ';
+            echo $staff["StaffName"];
+            echo ' ';
+            echo $staff["StartDate"];
+            echo ' ';
+            echo $staff["EndDate"];
+            echo '<br/>';
+        }
+    
+        $sql='SELECT bookings.BookingID, staff.StaffName, desks.DeskName, bookings.StartDate, bookings.EndDate FROM bookings INNER JOIN staff ON bookings.StaffID=staff.StaffID INNER JOIN desks ON bookings.DeskID=desks.DeskID';
+		$retval=mysqli_query($con,$sql);
+		if(!$retval){
+			die("Could not get data: ".mysqli_error());
+		}
+        echo "<br/>Bookings:<br/>";
+        foreach($retval as $staff){
+            echo $staff["BookingID"];
+            echo ' ';
+            echo $staff["StaffName"];
+            echo ' ';
+            echo "2F/",$staff["DeskName"];
+            echo ' ';
+            echo $staff["StartDate"];
+            echo ' ';
+            echo $staff["EndDate"];
+            echo '<br/>';
+        }
+    
 		
 		mysqli_close($con);
 		?>
