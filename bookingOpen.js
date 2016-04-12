c5682840ab211e3bd7ec8caf89f1105e21d4aaf2
benhowarth <benhowarthmail@gmail.com>
2016-04-12 19:09:19 +0100

    0.2.0
    Cleaned up and commented bookingOpen.js

--- bookingOpen.js
+++ bookingOpen.js
@@ -1,4 +1,5 @@
+//simple function for each button to run on click, it passes the id of the desk and it's seating group
 function openBook(deskID,defaultGroup){
-    //window.location.href="deskBookingForm.php?deskID="+deskID+"&defaultGroup="+defaultGroup;
+    //opens a new window, of 800 by 800, which contains the deskBookingForm, with the deskID and seating group passes into the url for use in the php
     window.open("deskBookingForm.php?deskID="+deskID+"&defaultGroup="+defaultGroup,"Ratting","width=800,height=800,toolbar=0,status=0,");
 }
